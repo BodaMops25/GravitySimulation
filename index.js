f61cbed3fe2714b86c6f59f3e20bb0ae73b576da
BodaMops25 <bodabodamops@gmail.com>
2024-08-17 15:54:41 +0200

    deleted comments

--- index.js
+++ index.js
@@ -111,10 +111,6 @@ function DrawEngine(context2D) {
   }
 }
 
-// drawBall(30, 30, 10)
-// drawVector(60, 30, 120, 60, 3, undefined, 0)
-// drawVector(150, 30, 60, 30, 3, undefined, 1)
-
 // ---- PARTICLES ----
 
 function Particle({coords = new Vec(), mass = 1, forces = new Vec(), radius = 10, color = '#fff'} = {}) {
@@ -125,17 +121,6 @@ function Particle({coords = new Vec(), mass = 1, forces = new Vec(), radius = 10
   this.color = color
 
   this.move = () => {
-    /* this.coords.set(coord => coord + this.force_x / this.mass * simulationSpeed)
-
-    this.x = this.x + this.force_x / this.mass * simulationSpeed
-    this.y = this.y + this.force_y / this.mass * simulationSpeed
-
-    const forces = this.forces.copy()
-    
-    forces.set(value => value / this.mass * simulationSpeed)
-
-    this.coords = this.coords.add(forces) */
-
     this.coords.set((coord, dim) => coord + this.forces[dim] / this.mass * simulationSpeed)
   }
 
@@ -158,14 +143,6 @@ function Camera({coords = new Vec(), scale = 1, particles, drawEngine} = {}) {
     return vec.copy().set((value, dim) => (value - this.coords[dim]) * this.scale + {x: cnvs.width, y: cnvs.height}[dim] / 2)
   }
 
-  // this.mapToCamera_x = x => {
-  //   return (x - this.x) * this.scale + cnvs.width / 2
-  // }
-
-  // this.mapToCamera_y = y => {
-  //   return (y - this.y) * this.scale + cnvs.height / 2
-  // }
-
   this.focus = vec => this.coords = vec.copy()
 
   this.render = () => {
@@ -175,12 +152,6 @@ function Camera({coords = new Vec(), scale = 1, particles, drawEngine} = {}) {
       const coords = this.mapToCameraCoords(particle.coords),
             forcesCoords = this.mapToCameraCoords(new Vec().set((value, dim) => particle.coords[dim] + particle.forces[dim] / particle.mass)),
             scale = particle.radius * this.scale
-
-      // const x = this.mapToCamera_x(particle.x),
-      //       y = this.mapToCamera_y(particle.y),
-      //       force_x = this.mapToCamera_x(particle.x + particle.force_x / particle.mass),
-      //       force_y = this.mapToCamera_y(particle.y + particle.force_y / particle.mass)
-      //       scale = particle.radius * this.scale
 
       this.drawEngine.drawBall(coords, scale < 5 ? 5 : scale, particle.color)
       this.drawEngine.drawVector(coords, forcesCoords, 2, '#fff', 0)
@@ -211,14 +182,9 @@ function Camera({coords = new Vec(), scale = 1, particles, drawEngine} = {}) {
 
   document.body.addEventListener("mousemove", event => {
     if(this.mousemove === true) {
-      /* const delta_x = event.offsetX - this.mouseDelta_x,
-            delta_y = event.offsetY - this.mouseDelta_y */
 
       const delta = new Vec(event.offsetX, event.offsetY).subtract(this.mouseDelta)
       this.coords = this.anchoredCoords.subtract(delta.divide(this.scale))
-
-      /* this.x = this.anchored_x - delta_x / this.scale
-      this.y = this.anchored_y - delta_y / this.scale */
     }
   })
 }
@@ -266,50 +232,3 @@ const loop = setInterval(() => {
   }
 
 }, 1000 / tickSpeed)
-
-// let GRAVITY_CONST = 6.674 * 1e-11,
-//     tickSpeed = 120, // count per second
-//     simulationSpeed = 1,
-//     particles = [],
-//     camera = new Camera(particles)
-
-// camera.scale = +sessionStorage['map_scale'] || 1
-// camera.x = +sessionStorage['map_x'] || 0
-// camera.y = +sessionStorage['map_y'] || 0
-
-// // particles.push(new Particle(10, 500, 2, 0, 11.9))
-// // particles.push(new Particle(-10000000, 0, 1, 0, 0.05))
-// // particles.push(new Particle(0, 0, 1e15, 0, 0, 500))
-
-// particles.push(new Particle(0, 0, 2e30, 0, 0, 7e8, 'yellow'))
-// particles.push(new Particle(150e9, 0, 6e24, 0, 30e3 * 6e24, 6e6, 'blue'))
-// particles.push(new Particle(150e9 + 380e6, 0, 7e22, 0, 1e3 * 7e22, 2e6, 'gray'))
-
-// const loop = setInterval(() => {
-//   ctx.clearRect(0, 0, cnvs.width, cnvs.height)
-
-//   for(const particle of particles) {
-
-//     for(const particle2 of particles) {
-//       if(particle !== particle2) {
-
-//         const r = distance(particle2.x, particle2.y, particle.x, particle.y),
-//               force = particle.mass * particle2.mass / r**2 * GRAVITY_CONST * simulationSpeed,
-//               angle = radToDeg(Math.atan2(particle2.x - particle.x, particle2.y - particle.y)),
-//               force_x = Math.sin(degToRad(angle)) * force,
-//               force_y = Math.cos(degToRad(angle)) * force
-
-//         particle.impulse(force_x, force_y)
-//       }
-//     }
-
-//     particle.move()
-
-//     camera.render()
-
-//     drawCursor()
-
-//     // particle.draw()
-//   }
-
-// }, 1000 / tickSpeed)
