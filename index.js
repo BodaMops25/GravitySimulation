ea1fb5a38e56680ea4f4aa57e920e05fcd12628e
Bohdan Konoplov <bodabodamops@gmail.com>
2024-11-12 11:09:08 +0100

    c

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
+const threshold = .5
+
+const directs = ['nw', 'ne', 'sw', 'se']
+
 const root = {},
-      points = [
-      {label: 'A', x: 0, y: 0, mass: 5},
-      {label: 'B', x: 6.3, y: 1.1, mass: 5},
-      {label: 'C', x: 5.6, y: 1.8, mass: 5},
-      {label: 'D', x: 10, y: 10, mass: 5}
+//       points = [
+//       {label: 'A', x: 0, y: 0, mass: 5},
+//       {label: 'B', x: 6.3, y: 1.1, mass: 5},
+//       {label: 'C', x: 5.6, y: 1.8, mass: 5},
+//       {label: 'D', x: 10, y: 10, mass: 5}
+// ]
+
+points = [
+  {label: 'A', x: 0, y: 0, mass: 1},
+  {label: 'B', x: 8, y: 0, mass: 1},
+  {label: 'C', x: 10, y: 0, mass: 1}
 ]
+
+function cartesianDistance(x1, y1, x2, y2) {
+  const d1 = (x1**2 + y1**2)**.5,
+        d2 = (x2**2 + y2**2)**.5
+
+  return d2 - d1
+}
 
 function getAreaCorners(points) {
   let min_x = points[0].x, min_y = points[0].y, max_x = points[0].x, max_y = points[0].y
@@ -21,6 +38,15 @@ function getAreaCorners(points) {
 }
 
 function foo(root, point, {min_x, min_y, max_x, max_y}) {
+  if(root.sectors === undefined) {
+    root.sectors = {}
+  }
+  if(root.data === undefined) {
+    root.data = {
+      scale: max_x - min_x
+    }
+  }
+
   const avarage_x = (max_x + min_x) / 2,
         avarage_y = (max_y + min_y) / 2
 
@@ -45,34 +71,58 @@ function foo(root, point, {min_x, min_y, max_x, max_y}) {
     new_coords.min_x = avarage_x
   }
 
-  if(root[sector] === undefined) {
-    root[sector] = point
+  const {sectors, data: rootData} = root
+  debugger
+  if(sectors[sector] === undefined) {
+    sectors[sector] = point
   }
-  else if(root[sector].label !== undefined) {
-    const point_tmp = root[sector]
-    root[sector] = {}
+  else if(sectors[sector].label !== undefined) {
+    const point_tmp = sectors[sector]
+    sectors[sector] = {}
 
-    foo(root[sector], point_tmp, new_coords)
-    foo(root[sector], point, new_coords)
+    foo(sectors[sector], point_tmp, new_coords)
+    foo(sectors[sector], point, new_coords)
   }
-  else if(root[sector].label === undefined) foo(root[sector], point, new_coords)
+  else if(sectors[sector].label === undefined) foo(sectors[sector], point, new_coords)
   
-  root.mass = 0
-  root.x = 0
-  root.y = 0
-  root.scale = max_x - min_x
+  rootData.mass = 0
+  rootData.x = 0
+  rootData.y = 0
 
-  for(const sectorKey in root) {
-    const sector = root[sectorKey]
-    if(sector.mass !== undefined) root.mass += sector.mass
-    if(sector.x !== undefined && sector.y !== undefined) {
-      root.x += sector.x * sector.mass
-      root.y += sector.y * sector.mass
-    }
+  for(const sectorKey in sectors) {
+    const sect = sectors[sectorKey],
+          sectMass = sect.mass !== undefined ? sect.mass : sect.data.mass,
+          sectX = sect.x !== undefined ? sect.x : sect.data.x,
+          sectY = sect.y !== undefined ? sect.y : sect.data.y
+
+    rootData.mass += sectMass
+    rootData.x += sectX * sectMass
+    rootData.y += sectY * sectMass
   }
 
-  root.x /= root.mass
-  root.y /= root.mass
+  rootData.x /= rootData.mass
+  rootData.y /= rootData.mass
+}
+
+function bodyForce(body, bodiesTree) {
+
+  for(const sectorKey in bodiesTree.sectors) {
+    const sector = bodiesTree.sectors[sectorKey]
+
+    if(body === sector) continue
+
+    if(sector.label !== undefined) {
+      console.log('from point', sector, 'to', body)
+      return
+    }
+    
+    const dist = cartesianDistance(body.x, body.y, sector.data.x, sector.data.y),
+        k = sector.data.scale / dist
+
+      if(k > threshold) console.log('from sectors', sector, 'to', body)
+
+      bodyForce(body, sector)
+  }
 }
 
 const coords = getAreaCorners(points)
@@ -82,3 +132,5 @@ for(const point of points) {
 }
 
 console.log(root)
+
+bodyForce(points[0], root)
