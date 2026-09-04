75111483b19eeb9d00ae77e011461e1c36a02dd5
BodaMops25 <bodabodamops@gmail.com>
2026-09-04 11:03:50 +0200

    clean src folder for new branch

--- src/types.ts
+++
@@ -1,6 +0,0 @@
-export type Vec = {x: number, y: number}
-export type PolarVec = {angle: number, magnitude: number}
-
-export type PhisMarks = {exp: number, mark: string}
-
-export type CanvasColor = string | CanvasGradient | CanvasPattern
